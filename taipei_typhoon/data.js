c7597b082accecb94b3abef603e5bdc8dcaaad11
kaicsii <60124552+kaicsii@users.noreply.github.com>
2026-07-10 00:43:12 +0800

    Add files via upload

--- taipei_typhoon/data.js
+++ taipei_typhoon/data.js
@@ -4,37 +4,37 @@ data = {
     "controls": 0,
     "items": [{
         "id": "z_fY1pj1VBw",
-        "title": "台北象山",
+        "title": "台北 象山 Mt. Elephant",
         "bgColor": "#E95242",
         "color": "white",
         "enabled": true
     },{
         "id": "Ndo_8RuefH4",
-        "title": "台北大稻埕",
+        "title": "台北 大稻埕 Dadaocheng",
         "bgColor": "#E95242",
         "color": "white",
         "enabled": true
     },{
         "id": "uvwfYzwY8tg",
-        "title": "台北碧山巖",
+        "title": "台北 碧山巖 Bishan Temple",
         "bgColor": "#E95242",
         "color": "white",
         "enabled": true
     },{
         "id": "fP4ecxfsJos",
-        "title": "台北大佳河濱公園",
+        "title": "台北 大佳河濱公園 Dajia Riverside Park",
         "bgColor": "#E95242",
         "color": "white",
         "enabled": true
     },{
         "id": "34h_Cy_n0Ok",
-        "title": "台北劍潭山微風平台",
+        "title": "台北 劍潭山微風平台 Breeze Observatory",
         "bgColor": "#E95242",
         "color": "white",
         "enabled": true
     },{
         "id": "215ahZ_0rTg",
-        "title": "台北貓空指南宮",
+        "title": "台北 貓空指南宮 Maokong Zhinan Temple",
         "bgColor": "#E95242",
         "color": "white",
         "enabled": true
